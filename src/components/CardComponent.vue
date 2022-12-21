b92f2ba4009a7add43002761b6cedb7f3798dd22
hdlopesrocha <hdlopesrocha@protonmail.com>
2022-12-21 11:26:38 +0000

    Moved products to arrays and extracted card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            {{ header }}
+        </div>
+        <div class="card-body">
+            <div v-html="body"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardComponent',
+    props: {
+        header: String,
+        body: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
